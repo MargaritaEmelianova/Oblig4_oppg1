0c7c385bbfc3e5b215d73a72236971a1cf84174f
MargaritaEm <mem004@post.uit.no>
2017-03-31 16:23:07 +0200

    have added counter for the character occurrence

--- Oblig4/src/SnuTekst.java
+++ Oblig4/src/SnuTekst.java
@@ -8,14 +8,30 @@ public class SnuTekst {
 System.out.print("Enter a string: ");
 String tekst = input.nextLine();
 System.out.print("The reverse of " + tekst + " is as follows: ");
-	baklengs(tekst);
+baklengs(tekst);
+System.out.print("     Enter the character to count: ");
+	char x = input.next().charAt(0);
+	System.out.print("The character " + x + " appears " + count(tekst, x) + " time(s).");
+	count(tekst, x);
+	
 	}
 
 
- public static void baklengs (String tekst){
+ public static int count(String tekst, char a) {
+		
+	 if (tekst.length() == 1)
+			return(tekst.charAt(0) == a ? 1 : 0);
+		return (tekst.charAt(0) == a ? 1 : 0) + count(tekst.substring(1, tekst.length()), a);
+	
+	
+ }
+
+
+public static void baklengs (String tekst){
 	 if ( tekst.length() !=0){
 		 System.out.print(tekst.charAt(tekst.length() - 1));
 	 baklengs(tekst.substring(0, tekst.length() - 1));
 	 }
+	 
  }
  }
